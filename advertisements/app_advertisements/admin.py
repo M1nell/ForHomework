5a6816acba0ec4f957e8b9438518bd3339cdec48
M1nell <Ivan.fhh58@gmail.com>
2023-08-03 21:30:24 +0300

    Homework_8

--- advertisements/app_advertisements/admin.py
+++ advertisements/app_advertisements/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import advertisement
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['title','descriptions','price','trade','date_now','date_update',]
+    list_filter = ['date_now','descriptions','trade']
+    fieldsets = (
+        ("First block",{
+            "fields":('title','descriptions')}),
+        ("Second block",{
+            "fields":('price','trades')}),
+    )
+    actions = ['make_true','make_false']
+    @admin.action(description="update (trade = True)")
+    def make_true(self,request,queryset):
+        queryset.update(trade= True)
+        pass
+
+    @admin.action(description="update (trade = False)")
+    def make_false(self,request,queryset):
+        queryset.update(trade= False)
+        pass
+
+admin.site.register(advertisement,AdvertisementAdmin)
 
 # Register your models here.
--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,16 +1,32 @@
 from django.db import models
+from django.utils.html import format_html
+from django.contrib import admin
 # Create your models here.
 class advertisement(models.Model):
     title = models.CharField("name",max_length=120)
     descriptions = models.TextField()
     price = models.DecimalField("price", max_digits=9, decimal_places=2)
     trade = models.BooleanField("trade", help_text="If you wanna trade")
-    date_now = models.DateField("Date of creation", auto_now_add= True)
-    date_update = models.DateField("Update date", auto_now= True)
+    date_now = models.DateTimeField("Date of creation", auto_now_add= True)
+    date_update = models.DateTimeField("Update date", auto_now= True)
 
-def __str__(self):
-    return f'advertisement(id = {self.id},title = {self.title}, price = {self.price} '
+    def __str__(self):
+        return f'advertisement(id = {self.id},title = {self.title}, price = {self.price} '
 
-class Meta:
-    db_table = 'advertisements'
+    class Meta:
+        db_table = 'advertisements'
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
+
+
+    # @admin.display(description="Dete on creation")
+    # def created_date(self):
+    #     from django.utils import timezone
+    #     if self.date_now.date() ==timezone.now().date():
+    #         created_time = self.date_now.time().strftime("%H:%M:%S")
+    #         return format_html(
+    #             '<span style = "color:green; font-weight: bold;"> Today at {}</span>',created_time
+    #         )
+    #     return self.darte_now.strftime("%d.%m.%y at %H:%M:%S")
+    #
 
